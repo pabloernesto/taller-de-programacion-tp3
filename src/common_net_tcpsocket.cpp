0746dbe799536bf51d0cc5d72d9ef1e90abacf58
Pablo Repetto <caussabon@gmail.com>
2017-10-08 16:53:44 -0300

    Revert "Implemented stream-like syntax for TCPSocket"
    This reverts commit f43614385a9b96727ab8fbc5bc59c66ed5b1d22a.

--- src/common_net_tcpsocket.cpp
+++ src/common_net_tcpsocket.cpp
@@ -20,33 +20,3 @@ int TCPSocket::send(const char* buffer, int lenght) {
 int TCPSocket::receive(char* buffer, int lenght) {
     return socket_receive(&(this->socket), buffer, lenght);
 }
-
-TCPSocket& operator<<(TCPSocket& os, const char val) {
-    os.send((char*)&val, sizeof(val));
-    return os;
-}
-
-TCPSocket& operator<<(TCPSocket& os, const int val) {
-    os.send((char*)&val, sizeof(val));
-    return os;
-}
-
-TCPSocket& operator<<(TCPSocket& os, const unsigned int val) {
-    os.send((char*)&val, sizeof(val));
-    return os;
-}
-
-TCPSocket& operator>>(TCPSocket& os, char& val) {
-    os.receive(&val, sizeof(char));
-    return os;
-}
-
-TCPSocket& operator>>(TCPSocket& os, int& val) {
-    os.receive((char*)&val, sizeof(int));
-    return os;
-}
-
-TCPSocket& operator>>(TCPSocket& os, unsigned int& val) {
-    os.receive((char*)&val, sizeof(unsigned int));
-    return os;
-}
