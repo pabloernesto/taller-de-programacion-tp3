b387b00bf879b28c5949edf6a1a264973a0499c7
Pablo Repetto <caussabon@gmail.com>
2017-10-11 19:23:38 -0300

    refactor: eliminated unnecesary 'this->'

--- src/server_database.cpp
+++ src/server_database.cpp
@@ -3,9 +3,16 @@
 
 using namespace std;
 
+/* For ease of synchronization, no public method calls another,
+ * directly or otherwise.
+ *
+ * When synchonization is added to the class, every public method will be
+ * mutexed, and recurring tasks will be offloaded into the non-mutexed private
+ * methods. */
+
 int DB::agregarMonto(unsigned int card, int sum) {
-    auto ptr = this->cards.find(card);
-    if (ptr == this->cards.end())
+    auto ptr = cards.find(card);
+    if (ptr == cards.end())
         throw runtime_error("Se intento modificar tarjeta no inicializada "
                 + to_string(card));
     if (sum < 0 && (ptr->second + sum) < 0)
@@ -15,32 +22,32 @@ int DB::agregarMonto(unsigned int card, int sum) {
 }
 
 int DB::forzarAgregarMonto(unsigned int card, int sum) {
-    auto ptr = this->cards.find(card);
-    if (ptr == this->cards.end())
+    auto ptr = cards.find(card);
+    if (ptr == cards.end())
         throw runtime_error("Se intento modificar tarjeta no inicializada "
                 + to_string(card));
     return ptr->second += sum;
 }
 
 int DB::consultarMonto(unsigned int card) {
-    auto ptr = this->cards.find(card);
-    if (ptr == this->cards.end())
+    auto ptr = cards.find(card);
+    if (ptr == cards.end())
         throw runtime_error("Se intento consultar monto de la tarjeta "
                 "no inicializada " + to_string(card));
     return ptr->second;
 }
 
 void DB::registrarTarjeta(unsigned int card) {
-    auto ptr = this->cards.find(card);
-    if (ptr != this->cards.end())
+    auto ptr = cards.find(card);
+    if (ptr != cards.end())
         throw runtime_error("Se intento registrar tarjeta existente "
                 + to_string(card));
-    this->cards[card] = 0;
+    cards[card] = 0;
 }
 
 void DB::asignarMonto(unsigned int card, int sum) {
-    auto ptr = this->cards.find(card);
-    if (ptr == this->cards.end())
+    auto ptr = cards.find(card);
+    if (ptr == cards.end())
         throw runtime_error("Se intento modificar tarjeta no inicializada "
                 + to_string(card));
     ptr->second = sum;
